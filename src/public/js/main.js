d9ca868c33cc4a094f1be14dcfc9d6165ce5c75b
mariosepulveda <masepulvedar@uqvirtual.edu.co>
2024-01-31 00:49:44 -0500

    segundo commit, creacion de usuarios y chat entre los mismos

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,5 +1,4 @@
 $(function (){
-    alert('works!!');
 
     const socket = io();
 
@@ -9,17 +8,58 @@ $(function (){
     const $messageBox = $('#message');
     const $chat = $('#chat');
 
-    // events 
+     // get DOM elements form nickname form
+
+     const $nickForm = $('#nickForm');
+     const $nickname = $('#nickname');
+     const $nickError = $('#nickError');
+     const $users = $('#usernames');
+    
+     // events 
+
+
+    $nickForm.submit( e => {
+        e.preventDefault();
+
+        socket.emit('new user',$nickname.val(), data => {
+            if(data){
+                $('#nickWrap').hide();
+                $('#contentWrap').show();
+            }else{
+                $nickError.html(`
+                <div class="alert alert-danger">
+                    that username already exists!!
+                </div>`)
+            }
+            $nickname.val('');
+        });
+
+    });
 
     $messageForm.submit( e => {
         e.preventDefault();
-        socket.emit('send message',$messageBox.val());
+        socket.emit('send message',$messageBox.val(),data => {
+            $char.append(`<p class="error">${data}</p>`)
+        });
         $messageBox.val('');
         //console.log("enviando datos",$messageBox.val());
     });
 
     socket.on('new message', function (data){
-        $chat.append(data+'<br/>')
+        $chat.append('<b>'+data.nick+'</b>: </br> '+data.msg+ '</br>')
     });
+
+    socket.on('usernames',data => {
+        let html = '';
+        
+        for(let i = 0; i<data.length;i++){
+            html += `<p><i class="fas fa-user"></i>${data[i]}</>`
+        }
+        $users.html(html);
+    });
+
+    socket.on('whisper', data => {
+        $chat.append(`<p class="whisper"><b>${data.nick}:</b> ${data.msg}</p>`);
+    })
 
 })
--- src/sockets.js
+++ src/sockets.js
@@ -1,10 +1,63 @@
 module.exports = function (io) {
 
+    let users = {
+
+    };
+
     io.on('connection', (socket) => {
         console.log('nuevo usuario conectado');
         
-        socket.on('send message', function(data){
-            io.sockets.emit('new message',data);
-        })
+        socket.on('new user', (data,cb) => {
+            if(data in users){
+                cb(false);
+            }else{
+                cb(true);
+                socket.nickname = data;
+                users[socket.nickname] = socket;
+                updateNickNames();            
+            }
+        });
+
+        socket.on('send message', (data,cb) => {
+
+            var msg = data.trim();
+
+            if(msg.substr(0,3) === '/w '){
+                msg = msg.substr(3);
+                const index = msg.indexOf(' ');
+                if(index !== -1){
+                    var name = msg.substring(0,index);
+                    var msg = msg.substing(index+1);
+                    if(name in users){ 
+                        users[name].emit('whisper',{
+                            msg,
+                            nick:socket.nickname
+                        });
+                    }else {
+                        cb('Error! Please enter a valid user');
+                    }
+                }else {
+                    cb('Error! Please enter your message')
+                }
+            }else {
+                io.sockets.emit('new message',{
+                    msg:data,
+                    nick:socket.nickname
+                });
+            }
+
+        });
+
+        socket.on('disconnect',data => {
+            if(!socket.nickname) return;
+            delete users[socket.nickname];
+            //nicknames.splice(nicknames.indexOf(socket.nickname),1);
+            updateNickNames();
+
+        });
+
+        function updateNickNames(){
+            io.sockets.emit('usernames',Object.keys(users));
+        }
     });
 }
